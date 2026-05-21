a709f5dba22ce74abeab8e5ddecdb56bbd130b80
bruno rojo <mr.rojo.bruno@gmail.com>
2026-05-22 01:16:31 +0200

    Mise en place des validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :comment, lenght: { minimum: 6 }
+  validates :movie_id, presence: { scope: :list_id }
 end
