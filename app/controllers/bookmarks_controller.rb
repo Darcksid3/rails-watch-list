182b72a266454cda3a4673a0cfb4f973b6a2a9fc
bruno rojo <mr.rojo.bruno@gmail.com>
2026-05-22 12:19:53 +0200

    fin des lise manque all CSS

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -7,16 +7,28 @@ class BookmarksController < ApplicationController
 
     def create
         @list = List.find(params[:list_id])
-        
+        @bookmark = Bookmark.new(params_bookmark)
+        @bookmark.list = @list
+        if @bookmark.save
+            redirect_to list_path(@list)
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @bookmark = set_bookmark
+        @bookmark.destroy
+        redirect_to lists_path, status: :see_other
     end
 
     private
 
     def set_bookmark
-        @bookmark = Bookmark.find(params(:id))
+        @bookmark = Bookmark.find(params[:id])
     end
 
     def params_bookmark
-        params.require(:bookmark).permit(:comment)
+        params.require(:bookmark).permit(:comment, :movie_id)
     end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,12 @@
 class ListsController < ApplicationController
-    before_action :set_list, only: [:show]
+    before_action :set_list, only: [:show, :destroy]
 
     def index
         @lists = List.all
     end
 
     def show
+        @bookmark = Bookmark.new
     end
 
     def new
@@ -13,9 +14,18 @@ class ListsController < ApplicationController
     end
 
     def create
+        @list = List.new(params_list)
+        if @list.save
+            redirect_to list_path(@list)
+        else
+            render :new, status: :unprocessable_entity
+        end
+
     end
 
     def destroy
+        @list.destroy
+        redirect_to lists_path, status: :see_other
     end
 
     private
